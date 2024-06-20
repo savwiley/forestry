8d8ad596093c0daddae050f76b84b7d9dda9dbcd
Savannaha <savwiley@yahoo.com>
2024-06-20 17:34:04 -0500

    add #header-pages to scrolling function

--- func.js
+++ func.js
@@ -1,10 +1,16 @@
 /* grab doms */
 const header = document.querySelector('#header');
+const headerPage = document.querySelector('#header-page');
 let scrolling = 0;
 
 /* scrolling event listener */
 window.addEventListener("scroll", () => {
   scrolling = window.scrollY;
+  if (scrolling >= 75) {
+    headerPage.style.background = "var(--accent1)";
+  } else {
+    headerPage.style.background = "var(--accent1O)";
+  }
   if (scrolling >= 100) {
     header.style.background = "var(--accent1)";
   } else {
